71fb5e7bc47c45eaa90092e026d885bf750fb907
yamasaki <yamasakipann0218@gmail.com>
2022-10-30 01:01:35 +0900

    sql インデントそろえた

--- server/database/mysql/sql/1_create_table.sql
+++ server/database/mysql/sql/1_create_table.sql
@@ -1,15 +1,15 @@
 CREATE TABLE `communities` (
-  `id`      serial       COLLATE utf8mb4_bin NOT NULL,
-  `name`    varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `img_url`  varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `content`  varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `id`        serial       COLLATE utf8mb4_bin NOT NULL,
+  `name`      varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `img_url`   varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `content`   varchar(255) COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE `users` (
-  `id`      serial       COLLATE utf8mb4_bin NOT NULL,
-  `name`    varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `id`        serial       COLLATE utf8mb4_bin NOT NULL,
+  `name`      varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `password`  varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `mail`  varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `mail`      varchar(255) COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;

f93ab8746a0d8cc79a0a78fb9d7e9218235db899
nisi0929 <kengo279@icloud.com>
2022-10-30 22:32:09 +0900

    front/feat:動的ルーティングの追加

--- client/pages/user/[name].tsx
+++ client/pages/user/[name].tsx
@@ -1,15 +1,17 @@
 import { Board } from '../../components/BaseParts/Board';
 import { TypoGraphy } from '../../components/BaseParts/TypoGraphy';
 import { Page } from '../../components/Wrapper/Page';
-
+import { useRouter } from 'next/router';
 const index = () => {
+  const router = useRouter();
+  const { name } = router.query;
   return (
     <div>
       <Page>
         <Board className="w-full md:w-1/2">
           <div className="p-5 text-center">
             <div className="m-auto my-3 h-32 w-32 rounded-full bg-orange-400"></div>
-            <TypoGraphy className="text-xl">ユーザー名</TypoGraphy>
+            <TypoGraphy className="text-xl">{name}</TypoGraphy>
             <div className="my-5 border-t-2 border-b-2 border-black py-5">
               <p>Eth:0.99</p>
               <p>いいね数:10</p>

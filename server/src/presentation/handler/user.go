a4328a2d881e0bfdff40bacd6f39f39b054715d5
yamasaki <yamasakipann0218@gmail.com>
2022-11-01 01:19:57 +0900

    handler 少し

--- server/src/presentation/handler/user.go
+++ server/src/presentation/handler/user.go
@@ -1,6 +1,22 @@
 package handler
 
+import "github.com/kuma0328/web3hackathon/usecase"
+
+type UserHandler struct {
+	uc usecase.IUserUsecase
+}
+
+func NewUserHandler(uc usecase.IUserUsecase) *UserHandler {
+	return &UserHandler{
+		uc: uc,
+	}
+}
+
 type userJson struct {
+	Id       int    `json:"id"`
+	Name     string `json:"name"`
+	Mail     string `json:"mail"`
+	Password string `json:"password"`
 }
 
 type usersJson []userJson

284c121e7913a5fff4169a379d34d8f36fbe13c3
yamasaki <yamasakipann0218@gmail.com>
2022-11-01 22:33:04 +0900

    handler user

--- server/src/presentation/handler/user.go
+++ server/src/presentation/handler/user.go
@@ -1,6 +1,10 @@
 package handler
 
-import "github.com/kuma0328/web3hackathon/usecase"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/kuma0328/web3hackathon/domain/entity"
+	"github.com/kuma0328/web3hackathon/usecase"
+)
 
 type UserHandler struct {
 	uc usecase.IUserUsecase
@@ -12,6 +16,13 @@ func NewUserHandler(uc usecase.IUserUsecase) *UserHandler {
 	}
 }
 
+func (u *UserHandler) Sigunup(ctx *gin.Context) {
+	newName := ctx.Param("name")
+	newMail := ctx.Param("mail")
+	newMail := ctx.Param("password")
+
+}
+
 type userJson struct {
 	Id       int    `json:"id"`
 	Name     string `json:"name"`
@@ -20,3 +31,21 @@ type userJson struct {
 }
 
 type usersJson []userJson
+
+func userEntityToJson(c *entity.User) userJson {
+	return userJson{
+		Id:       c.Id,
+		Name:     c.Name,
+		Mail:     c.Mail,
+		Password: c.Password,
+	}
+}
+
+func userJsonToEntity(j *userJson) *entity.User {
+	return &entity.User{
+		Id:       j.Id,
+		Name:     j.Name,
+		Mail:     j.Mail,
+		Password: j.Password,
+	}
+}

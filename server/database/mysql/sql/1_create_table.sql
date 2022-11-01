d5da06ade647f897b652eea2bb4e00242e9855f1
yamasaki <yamasakipann0218@gmail.com>
2022-11-01 22:33:48 +0900

    sql 変更

--- server/database/mysql/sql/1_create_table.sql
+++ server/database/mysql/sql/1_create_table.sql
@@ -7,7 +7,7 @@ CREATE TABLE `communities` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE `users` (
-  `id`        serial       COLLATE utf8mb4_bin NOT NULL,
+  `id`        int          COLLATE utf8mb4_bin NOT NULL AUTO_INCREMENT,
   `name`      varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `password`  varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `mail`      varchar(255) COLLATE utf8mb4_bin NOT NULL,

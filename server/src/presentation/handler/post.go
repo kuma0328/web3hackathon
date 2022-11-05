e3b1f5f53821ab9f536e4c6f4ddc53d7655ea425
yamasaki <yamasakipann0218@gmail.com>
2022-11-05 18:34:30 +0900

    handler

--- server/src/presentation/handler/post.go
+++ server/src/presentation/handler/post.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 
@@ -20,7 +22,7 @@ func NewPostHandler(uc usecase.IPostUsecase) *PostHandler {
 }
 
 func (h *PostHandler) GetPostById(ctx *gin.Context) {
-	idString := ctx.Param("community_id")
+	idString := ctx.Param("id")
 	id, err := strconv.Atoi(idString)
 	if err != nil {
 		ctx.JSON(
@@ -38,15 +40,145 @@ func (h *PostHandler) GetPostById(ctx *gin.Context) {
 		)
 		return
 	}
+	ctx.Data(http.StatusOK, "image/jpeg", post.Img)
+
+	postJson := postEntityToJson(post)
+	ctx.JSON(
+		http.StatusOK,
+		gin.H{"data": postJson},
+	)
 
 }
 func (h *PostHandler) UpdatePostOfId(ctx *gin.Context) {
+	img, _, err := ctx.Request.FormFile("img")
+	if err != nil {
+		ctx.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": err.Error()},
+		)
+		return
+	}
+	source, _ := io.ReadAll(img)
+
+	newContent := ctx.Request.FormValue("content")
+	newCommunityIdString := ctx.Request.FormValue("community_id")
+	newCommunityId, err := strconv.Atoi(newCommunityIdString)
+	if err != nil {
+		ctx.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": err.Error()},
+		)
+		return
+	}
+
+	newUserIdString := ctx.Request.FormValue("user_id")
+	newUserId, err := strconv.Atoi(newUserIdString)
+	if err != nil {
+		ctx.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": err.Error()},
+		)
+		return
+	}
+
+	newIdString := ctx.Request.FormValue("id")
+	newId, err := strconv.Atoi(newIdString)
+	if err != nil {
+		ctx.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": err.Error()},
+		)
+		return
+	}
+
+	post := &entity.Post{}
+	post.Id = newId
+	post.Content = newContent
+	post.CommunityId = newCommunityId
+	post.UserId = newUserId
+	post.Img = source
+
+	post, err = h.uc.UpdatePostOfId(ctx, post)
+	ctx.Data(http.StatusOK, "image/jpeg", post.Img)
+
+	postJson := postEntityToJson(post)
+	ctx.JSON(
+		http.StatusOK,
+		gin.H{"data": postJson},
+	)
 
 }
 func (h *PostHandler) DeletePostOfId(ctx *gin.Context) {
+	idString := ctx.Param("id")
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		ctx.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": err.Error()},
+		)
+		return
+	}
+
+	err = h.uc.DeletePostOfId(ctx, id)
+	if err != nil {
+		ctx.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": err.Error()},
+		)
+		return
+	}
+	ctx.JSON(
+		http.StatusOK,
+		gin.H{"ok": fmt.Sprintf("success delete community ( id : %d )", id)},
+	)
 
 }
 func (h *PostHandler) CreateNewPost(ctx *gin.Context) {
+	img, _, err := ctx.Request.FormFile("img")
+	if err != nil {
+		ctx.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": err.Error()},
+		)
+		return
+	}
+	source, _ := io.ReadAll(img)
+
+	newContent := ctx.Request.FormValue("content")
+	newCommunityIdString := ctx.Request.FormValue("community_id")
+	newCommunityId, err := strconv.Atoi(newCommunityIdString)
+	if err != nil {
+		ctx.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": err.Error()},
+		)
+		return
+	}
+
+	newUserIdString := ctx.Request.FormValue("user_id")
+	newUserId, err := strconv.Atoi(newUserIdString)
+	if err != nil {
+		ctx.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": err.Error()},
+		)
+		return
+	}
+
+	post := &entity.Post{}
+	post.Content = newContent
+	post.CommunityId = newCommunityId
+	post.UserId = newUserId
+	post.Img = source
+
+	post, err = h.uc.CreateNewPost(ctx, post)
+	ctx.Data(http.StatusOK, "image/jpeg", post.Img)
+
+	postJson := postEntityToJson(post)
+	ctx.JSON(
+		http.StatusOK,
+		gin.H{"data": postJson},
+	)
 
 }
 
---
+++ server/src/presentation/router/post.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"github.com/kuma0328/web3hackathon/infrastructure/database"
+	"github.com/kuma0328/web3hackathon/infrastructure/persistance"
+	"github.com/kuma0328/web3hackathon/presentation/handler"
+	"github.com/kuma0328/web3hackathon/usecase"
+)
+
+func (r Router) InitPostRouter(conn *database.Conn) {
+	repo := persistance.NewPostRepository(conn)
+	uc := usecase.NewPostUsecase(repo)
+	h := handler.NewPostHandler(uc)
+
+	g := r.Engine.Group("/post")
+	g.GET("/:id", h.GetPostById)
+	g.PUT("/update", h.UpdatePostOfId)
+	g.DELETE("/id", h.DeletePostOfId)
+	g.POST("/create", h.CreateNewPost)
+}
